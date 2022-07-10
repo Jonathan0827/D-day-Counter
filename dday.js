2b5eb1df22c5e372144ce0443899a21a4b89bf6f
Patnekuv Ros (Junhyeop Lim) <limjunehyeop@gmail.com>
2022-07-10 18:46:46 +0900

    Basic features

---
+++ dday.js
@@ -0,0 +1,17 @@
+var dday = new Date("June 7, 2022, 02:00:00").getTime();
+
+setInterval(function () {
+	var today = new Date().getTime();
+	var gap = dday - today;
+	var d = Math.floor(gap / (1000 * 60 * 60 * 24));
+	var h = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60) - 1);
+	var m = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60) - 1);
+	var s = Math.ceil((gap % (1000 * 60)) / 1000 - 1);
+	if (gap <= 0) {
+		document.getElementById("count").innerHTML = "<h2>No D-day to view.";
+	} else {
+		document.getElementById(
+			"count"
+		).innerHTML = `<h2>${d} days ${h} hours ${m} minutes ${s} seconds left untill`;
+	}
+}, 1);
